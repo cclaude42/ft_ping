d73697629ddd6d6f4ba54c2caf88ce2aa9f59a40
Cesar Claude <cesarclaudepro@gmail.com>
2023-01-09 11:55:53 +0100

    :white_check_mark: revamped script

--- test.sh
+++ test.sh
@@ -2,10 +2,7 @@
 make
 
 echo && echo "Running ping..."
-timeout 2.5 ping $1 > out1.txt
+timeout 2.5 ping $@
 
 echo && echo "Running ft_ping..."
-sudo timeout 2.5 ./ft_ping $1 > out2.txt
-
-echo && echo "Diff :"
-diff out1.txt out2.txt
+sudo timeout 2.5 ./ft_ping $@
